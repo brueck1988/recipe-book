c10dc2b5f2bfc180bd4c0307882a762df6ff8d5a
alexander.brueck <alexander.brueck@credera.com>
2022-12-14 10:33:28 -0700

    Add logic to resolver to only fetch recipes if there currently are none

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './recipe.service';
 import { DataStorageService } from './../shared/data-storage.service';
 import { Recipe } from './recipe.model';
 import { Injectable } from '@angular/core';
@@ -8,9 +9,15 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/r
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
