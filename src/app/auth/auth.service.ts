284f7d5d3ecbbec7c2f0e99c4b68560a09c22665
alexander.brueck <alexander.brueck@credera.com>
2022-12-15 16:13:33 -0700

    set user for sign up and sign in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -17,9 +18,12 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
+    console.log("user IS");
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCHR0QCz9UNB-RE1F-zXz3OeKgs9G1oVhE',
       { 
@@ -27,7 +31,12 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-     ).pipe(catchError(this.handleError));
+     ).pipe(catchError(this.handleError), tap(resData => this.handleAuthentication(
+      resData.email, 
+      resData.localId, 
+      resData.idToken, 
+      +resData.expiresIn
+     )));
   }
 
   login(email: string, password: string) {
@@ -38,7 +47,24 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-      ).pipe(catchError(this.handleError));
+      ).pipe(catchError(this.handleError), tap(resData => this.handleAuthentication(
+        resData.email, 
+        resData.localId, 
+        resData.idToken, 
+        +resData.expiresIn
+       )));
+  }
+
+  private handleAuthentication(email: string, userId: string token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(
+      email, 
+      userId, 
+      token, 
+      expirationDate
+      );
+      console.log("user IS" + this.user);
+      this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
