cb5b6f9ce2fc34cbf0f7ee69df3c4bf3ba5b8f84
alexander.brueck <alexander.brueck@credera.com>
2022-12-21 19:08:08 -0500

    Set up auth reducer and actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,3 +1,4 @@
+import * as AuthActions from './auth.actions';
 import { User } from './../user.model';
 
 export interface State {
@@ -8,6 +9,24 @@ const initialState: State = {
 	user: null
 }
 
-export function authReducer(state: State = initialState, action) { 
-	return state;
+export function authReducer(state: State = initialState, action: AuthActions.AuthActions) { 
+	switch (action.type) {
+		case AuthActions.LOGIN:
+			const user = new User(
+				action.payload.email, 
+				action.payload.userId, 
+				action.payload.token, 
+				action.payload.expirationDate);
+			return {
+				...state,
+				user: user
+			};
+		case AuthActions.LOGOUT:
+			return {
+				...state,
+				user: null
+			};
+		default:
+			return state;
+	}
 }
