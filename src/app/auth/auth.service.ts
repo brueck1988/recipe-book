d5b79247514d5840a7aa31f6f0ea81f97550fbb7
alexander.brueck <alexander.brueck@credera.com>
2023-01-05 11:40:53 -0700

    Update service to use ngrx

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,9 @@ import { catchError, tap } from 'rxjs/operators';
 import { throwError, Subject, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
 import { environment } from 'src/environments/environment';
-
+import { Store } from '@ngrx/store';
+import * as fromApp from './../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 export interface AuthResponseData {
   kind: string;
   idToken: string;
@@ -23,7 +25,7 @@ export class AuthService {
   user = new BehaviorSubject<User>(null);
   private tokenExpirationTimer: any;
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, private store: Store<fromApp.AppState>) { }
 
   signup(email: string, password: string) {
     console.log("user IS");
@@ -64,15 +66,23 @@ export class AuthService {
       return;
     }
     const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
     if (loadedUser.token) {
-      this.user.next(loadedUser);
+      // this.user.next(loadedUser); // Replaced by NgRx
+      this.store.dispatch(new AuthActions.Login({ // Replaces code above
+        email: loadedUser.email, 
+        userId: loadedUser.id, 
+        token: loadedUser.token, 
+        expirationDate: new Date(userData._tokenExpirationDate)
+      }));
       const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
       this.autoLogout(expirationDuration);
     }
   }
 
   logout() {
-    this.user.next(null);
+    // this.user.next(null); // Replaced by NgRx
+    this.store.dispatch(new AuthActions.Logout()); // Replaces code above
     this.router.navigate(['/auth']);
     localStorage.removeItem('userData');
     if (this.tokenExpirationTimer) {
@@ -90,7 +100,13 @@ export class AuthService {
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
-    this.user.next(user);
+    // this.user.next(user); // Replaced by NgRx
+    this.store.dispatch(new AuthActions.Login({ // Replaces code above
+      email: email, 
+      userId: userId, 
+      token: token, 
+      expirationDate: expirationDate
+    }));
     this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
